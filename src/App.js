cf8b7b898db7a757d3feaa0555280829f03d0179
HamdiNur <hamdoonur26@gmail.com>
2024-07-24 05:47:17 +0300

    Handling error timer function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import Footer from "./Components/Footer";
 import TypingBox from "./Components/TypingBox";
 import { useTheme } from "./Context/ThemeContext";
 import { GlobalStyles } from "./Styles/global";
-import { auth } from "./firebaseConfig";
-import Header from "./Components/Header";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import UserPage from "./Pages/UserPage";
-import Alert from "./Components/Alert";
-import ComparePage from "./Pages/ComparePage";
+
 
 function App() {
 
