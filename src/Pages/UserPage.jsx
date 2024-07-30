92db8008babab941461479598b4052f6ea338856
HamdiNur <hamdoonur26@gmail.com>
2024-07-30 06:17:27 +0300

    Fetching Data from firestore

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -59,7 +59,13 @@ const UserPage = () => {
     }
 
   return (
-    
+    <div className='canvas'>
+        <UserInfo totalTestTaken={data.length}/>
+        <div className="graph">
+            <Graph graphData={graphData} type='date'/>
+        </div>
+        <ResultTable data={data}/>
+    </div>
   )
 }
 
