33d1f2043de158dff8cd2651f34427b4ecd87b7d
HamdiNur <hamdoonur26@gmail.com>
2024-07-30 19:16:52 +0300

    icon typing added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import AccountIcon from './AccountIcon'
 import CompareButton from './CompareButton'
-// import LOGO from '../pastille-methode.svg'
+import LOGO from '../pastille-methode.svg'
 
 const Header = () => {
   return (
     <div className="header">
         <div className="logo">
             
-              {/* <img className='logo-image' src={LOGO}/> */}
+              <img className='logo-image' src={LOGO}/>
+             
             
             <div style={{marginLeft:'-80px', marginTop: '15px'}}>
               <CompareButton/>
