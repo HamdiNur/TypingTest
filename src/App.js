e84bd7b1ceb1f7e857de3bfd5ce01f2c8b91c77e
HamdiNur <hamdoonur26@gmail.com>
2024-07-30 18:24:57 +0300

    imported comparepage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import UserPage from "./Pages/UserPage";
 import Alert from "./Components/Alert";
-
+import ComparePage from "./Pages/ComparePage";
 
 function App() { 
 
